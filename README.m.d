45cb12d02ea1fb74cc1ac01085515b78b5e878bb
Simonavet <simona.hassanein@gmail.com>
2023-03-23 00:27:47 +0200

    readme file added

---
+++ README.m.d
@@ -0,0 +1,5 @@
+•	LIMBAJ DE PROGRAMARE FOLOSIT - Python cu IDE PyCharm Community Edition 2022.1.3 - download: https://www.jetbrains.com/pycharm/download/#section=windows .
+•	PROIECTUL ÎN GITHUB : https://github.com/Simonavet/API_Project_Simple_Books.git
+•	CLONAREA DIN GITHUB – deschidem GIT BASH în folder-ul unde vrem să salvăm proiectul copiem URL din GITHUB -> mergem înapoi în GIT BASH -> comanda git clone -> click dreapta -> Paste -> Enter
+•	LIBRĂRII FOLOSITE ÎN IDE - requests și pytest - instalare din Terminal folosind - pip install requests și pip install pytest
+•	RULAREA TESTELOR – în Terminal – scriem – pytest nume_folder -v (pentru toate testele dintr-un folder) sau pytest nume_folder/nume_file -v (pentru testele dintr-un anumit file)
